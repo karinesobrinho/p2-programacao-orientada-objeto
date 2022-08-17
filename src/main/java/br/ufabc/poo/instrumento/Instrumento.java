71cfd4d5d2aba87ab3dd4a97f42cac9599abd462
Karine da Silva Sobrinho <karinedasilvasobrinho@MacBook-Air-de-Administrador.local>
2022-08-17 14:35:20 -0300

    init another class

--- src/main/java/br/ufabc/poo/instrumento/Instrumento.java
+++ src/main/java/br/ufabc/poo/instrumento/Instrumento.java
@@ -1,4 +1,29 @@
 package main.java.br.ufabc.poo.instrumento;
 
-public class Instrumento {
+import java.util.ArrayList;
+
+public abstract class Instrumento {
+    private String marca;
+    private long numeroLoteFabricacao;
+
+    public Instrumento(String marca, long numeroLoteFabricacao) {
+        this.marca = marca;
+        this.numeroLoteFabricacao = numeroLoteFabricacao;
+    }
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+
+    public long getNumeroLoteFabricacao() {
+        return numeroLoteFabricacao;
+    }
+
+    public void setNumeroLoteFabricacao(long numeroLoteFabricacao) {
+        this.numeroLoteFabricacao = numeroLoteFabricacao;
+    }
 }
