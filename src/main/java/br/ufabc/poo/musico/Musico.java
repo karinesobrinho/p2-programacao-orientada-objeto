2d2bb7eb6fb0ab566ededc78242afcdb2453be63
Karine da Silva Sobrinho <karinedasilvasobrinho@MacBook-Air-de-Administrador.local>
2022-08-17 14:33:33 -0300

    init musician class

--- src/main/java/br/ufabc/poo/musico/Musico.java
+++ src/main/java/br/ufabc/poo/musico/Musico.java
@@ -1,4 +1,31 @@
 package main.java.br.ufabc.poo.musico;
 
+import main.java.br.ufabc.poo.instrumento.Instrumento;
+
+import java.util.ArrayList;
+
 public class Musico {
+    private String nome;
+    private ArrayList<Instrumento> instrumentos;
+
+    public Musico(String nome, ArrayList<Instrumento> instrumentos) {
+        this.nome = nome;
+        this.instrumentos = instrumentos;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public ArrayList<Instrumento> getInstrumentos() {
+        return instrumentos;
+    }
+
+    public void setInstrumentos(ArrayList<Instrumento> instrumentos) {
+        this.instrumentos = instrumentos;
+    }
 }
